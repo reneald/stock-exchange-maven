437654fe2945683fd452d1b184cdabec62783f16
reneald <delea.vanreeth@gmail.com>
2018-03-12 14:08:25 +0100

    more setup, implemented ExternalStockInformationService

--- interfaces/src/main/java/be/reneald/interfaces/ExternalStockInformationService.java
+++ interfaces/src/main/java/be/reneald/interfaces/ExternalStockInformationService.java
@@ -1,4 +1,10 @@
 package be.reneald.interfaces;
 
+import java.math.BigDecimal;
+
 public class ExternalStockInformationService {
+    public BigDecimal getPriceInEuroForStock(String stockId) {
+        double randomPrice = Math.random() * 100;
+        return new BigDecimal(randomPrice < 10 ? 10 : randomPrice);
+    }
 }
